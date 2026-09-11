2a20ea5dc0d867389bde78cae84e356f3fbcfa36
Lekitha <leki.shetty@gmail.com>
2026-09-11 22:50:05 +0200

    Build portfolio as a list of holdings

--- main.py
+++ main.py
@@ -4,5 +4,11 @@ def create_holding(ticker, shares, cost_basis):
     "shares": shares,
     "cost_basis": cost_basis
 } 
+portfolio=[]
 holding1 = create_holding(ticker="AAPL", shares=10, cost_basis=150.00)
-print(holding1 )
+holding2 = create_holding(ticker="MSFT", shares=20, cost_basis=250.00)
+holding3 = create_holding(ticker="GOOG", shares=30, cost_basis=350.00)
+portfolio.append(holding1)
+portfolio.append(holding2)
+portfolio.append(holding3)
+print(portfolio)
